7dbb64daaa6929f6628696f9cc5041fe525c0b1a
WalterOsoy <31348535+WalterOsoy@users.noreply.github.com>
2022-07-09 18:55:07 -0600

    spotify endpoints

---
+++ functions/getGenres/lambda_function.py
@@ -0,0 +1,15 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+client_id='68afa0d8ff0b4751b1e7ce547502cba7'
+client_secret='a02fa9b3f79d4325bc275c4304b16cf4'
+
+sp= spotipy.Spotify (auth_manager=SpotifyClientCredentials(client_id=client_id, client_secret=client_secret))
+
+
+def getGenres():
+    result = sp.recommendation_genre_seeds()
+    return {"statusCode": 200, "body": result}
+
+def lambda_handler(event, context):
+    return getGenres()
