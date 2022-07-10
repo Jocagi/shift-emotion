c8aff51f969b311acb6b6dd2e03c4ed12406162a
sergio.lara-at-405522164029 <srgiokun@gmail.com>
2022-07-10 16:42:16 -0600

    Commit

--- deploy/api_gateway_methods.tf
+++ deploy/api_gateway_methods.tf
@@ -798,7 +798,7 @@ resource "aws_api_gateway_integration" "lambda_getRecomendations" {
   rest_api_id             = aws_api_gateway_rest_api.api_gateway.id
   resource_id             = aws_api_gateway_resource.spotify_recomendations.id
   http_method             = aws_api_gateway_method.spotify_recomendations_get.http_method
-  integration_http_method = aws_api_gateway_method.spotify_recomendations_get.http_method
+  integration_http_method = "POST"
 
   type = "AWS"
   content_handling     = "CONVERT_TO_TEXT"
